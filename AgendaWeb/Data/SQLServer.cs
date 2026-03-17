ba710007ff5fb960f9490f09de6ce606d3aef933
AntonioRogel <63703411+AntonioRogelUK@users.noreply.github.com>
2026-03-17 07:29:05 -0700

    Update SQLServer.cs

--- AgendaWeb/Data/SQLServer.cs
+++ AgendaWeb/Data/SQLServer.cs
@@ -1,31 +1,18 @@
-﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace AgendaWeb.Data
 {
-public class SQLServer
+    public class SQLServer
     {
         private readonly string _connectionString;
-
         public SQLServer(string connectionString)
         {
             _connectionString = connectionString;
         }
-
         public SqlConnection GetConnection()
         {
             return new SqlConnection(_connectionString);
         }
-
-        public async Task<SqlTransaction> CrearTransactionAsync(SqlConnection connection)
-        {
-            ArgumentNullException.ThrowIfNull(connection, nameof(connection));
-            if (connection.State != ConnectionState.Open)
-            {
-                await connection.OpenAsync();
-            }
-            return (SqlTransaction)await connection.BeginTransactionAsync();
-        }
-
         public async Task<int> NonQueryAsync(string query, SqlParameter[] parameters = null)
         {
             await using var connection = GetConnection();
@@ -37,25 +24,6 @@ public class SQLServer
             await connection.OpenAsync();
             return await command.ExecuteNonQueryAsync();
         }
-        public async Task<int> NonQueryAsync(SqlConnection connection, SqlTransaction transaction, string query, SqlParameter[] parameters = null)
-        {
-            ArgumentNullException.ThrowIfNull(connection, nameof(connection));
-
-            await using var command = new SqlCommand(query, connection, transaction);
-
-            if (parameters != null)
-            {
-                command.Parameters.AddRange(parameters);
-            }
-
-            if (connection.State != ConnectionState.Open)
-            {
-                await connection.OpenAsync();
-            }
-
-            return await command.ExecuteNonQueryAsync();
-        }
-
         public async Task<T> ScalarAsync<T>(string query, SqlParameter[] parameters = null)
         {
             await using SqlConnection sqlConnection = GetConnection();
@@ -73,33 +41,6 @@ public class SQLServer
             object result = await sqlCommand.ExecuteScalarAsync();
 
             return result is T value ? value : default;
-        }
-
-        public async Task<T> ScalarAsync<T>(SqlConnection connection, SqlTransaction transaction, string query, SqlParameter[] parameters = null)
-        {
-            ArgumentNullException.ThrowIfNull(connection, nameof(connection));
-            await using SqlCommand sqlCommand = new(query, connection, transaction)
-            {
-                CommandType = CommandType.Text
-            };
-
-            if (parameters is not null)
-            {
-                sqlCommand.Parameters.AddRange(parameters);
-            }
-
-            if (connection.State != ConnectionState.Open)
-            {
-                await connection.OpenAsync();
-            }
-
-
-            object result = await sqlCommand.ExecuteScalarAsync();
-
-            if (result is null || result is DBNull)
-                return default;
-
-            return (T)Convert.ChangeType(result, typeof(T));
         }
         public async Task<T> ReaderAsync<T>(string query, SqlParameter[] parameters = null) where T : class, new()
         {
