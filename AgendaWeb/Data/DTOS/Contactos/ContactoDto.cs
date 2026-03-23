1b0c861777dba108d1734ff1a492d771f076eedd
Camila Fimbres <camifimbres@gmail.com>
2026-03-23 07:57:18 -0700

    Actualizando Services,Command y SQL Server

---
+++ AgendaWeb/Data/DTOS/Contactos/ContactoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendaWeb.Data.DTOS.Contactos
+{
+    public class ContactoDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
